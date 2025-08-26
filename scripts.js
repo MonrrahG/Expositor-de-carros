9d220ec0d3f962b13d66f51837e11f9e3d4f1958
MonrrahG <ramonfoliveira2006@gmail.com>
2025-08-25 21:42:23 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,41 @@
+let prevButton = document.getElementById('prev');
+let nextButton = document.getElementById('next');
+let container = document.querySelector('.container');
+let items = container.querySelectorAll('.item');
+let indicator = document.querySelector('.indicators');
+let dots = indicator.querySelectorAll('ul li');
+let list = container.querySelector('.list')
+
+let active = 0;
+let firstPosition = 0;
+let lastPosition = items.length - 1;
+
+nextButton.onclick = () => {
+    list.style.setProperty('--calculation', 1)
+    let itemOld = container.querySelector('.item.active');
+    itemOld.classList.remove('active');
+
+    active = (active + 1 > lastPosition) ? 0 : active + 1;
+    items[active].classList.add('active');
+    updateDots();
+
+    indicator.querySelector('.number').innerHTML = '0' + (active + 1)
+}
+
+prevButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
+    let itemOld = container.querySelector('.item.active');
+    itemOld.classList.remove('active');
+
+    active = (active - 1 < firstPosition) ? lastPosition : active - 1;
+    items[active].classList.add('active');
+    updateDots();
+
+    indicator.querySelector('.number').innerHTML = '0' + (active + 1)
+}
+
+function updateDots() {
+    dots.forEach((dot, index) => {
+        dot.classList.toggle('active', index === active);
+    });
+}
